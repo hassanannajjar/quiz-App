823bb0db1233bf17e405ac63ae2885d189e240c8
ha77an <ha77an19@gmail.com>
2020-07-15 16:06:21 +0300

    added levels files

---
+++ levels/level_1.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const level_1 = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default level_1;
+
+const styles = StyleSheet.create({});
---
+++ levels/level_2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const level_2 = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default level_2;
+
+const styles = StyleSheet.create({});
