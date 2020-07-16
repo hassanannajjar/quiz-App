27c20dbe4a3b47e3c61fc6aaab74cc41f6736982
ha77an <ha77an19@gmail.com>
2020-07-16 09:15:56 +0300

    render all question and added next question

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -1,14 +1,68 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
+import Button from "../components/Button";
+import questions from "../data/questions";
+const Quiz = ({ navigation }) => {
+  const totalCount = questions.length;
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
 
-const Quiz = () => {
+  const NextQuestion = () => {
+    const nextIndex = activeQuestionIndex + 1;
+    if (nextIndex >= totalCount) {
+      return navigation.popToTop();
+    }
+    setActiveQuestionIndex(nextIndex);
+  };
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{questions[activeQuestionIndex].question}</Text>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.safearea}>
+        <View style={styles.buttonContainer}>
+          {questions[activeQuestionIndex].answers.map((answer) => {
+            return (
+              <Button
+                key={answer.id}
+                title={answer.text}
+                onPress={NextQuestion}
+              />
+            );
+          })}
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
 
 export default Quiz;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#429",
+    flex: 1,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 25,
+    textAlign: "center",
+    fontWeight: "600",
+    marginTop: "20%",
+  },
+  safearea: {
+    flex: 1,
+    marginTop: 40,
+    justifyContent: "space-between",
+  },
+  buttonContainer: {
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  score: {
+    position: "absolute",
+    top: 5,
+    fontSize: 30,
+    left: 30,
+  },
+});
